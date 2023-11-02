219218c0af7077ddc8189c678fed40f12396934d
RazatAggarwal <razat.aggarwal@outlook.com>
2023-11-02 16:15:13 +0530

    added telescoping constructor pattern

---
+++ LLD-One/src/main/java/com/lld/one/b_access_modifier_constructor/Client.java
@@ -0,0 +1,5 @@
+package com.lld.one.b_access_modifier_constructor;
+
+public class Client {
+
+}
---
+++ LLD-One/src/main/java/com/lld/one/b_access_modifier_constructor/telescoping_constructor_pattern/MainClient.java
@@ -0,0 +1,8 @@
+package com.lld.one.b_access_modifier_constructor.telescoping_constructor_pattern;
+
+public class MainClient {
+    public static void main(String[] args){
+        Student s = new Student(1,"Razat",'M');
+        System.out.println(s);
+    }
+}
