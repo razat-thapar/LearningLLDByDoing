5aec5ee7defe624e7e6b59ce97cc6e4208381584
RazatAggarwal <razat.aggarwal@outlook.com>
2023-11-27 15:47:22 +0530

    static block

--- LLD-One/src/main/java/com/lld/one/d_interfaces_abstractclasses_static/static_keyword/Main.java
+++ LLD-One/src/main/java/com/lld/one/d_interfaces_abstractclasses_static/static_keyword/Main.java
@@ -1,9 +1,12 @@
 package com.lld.one.d_interfaces_abstractclasses_static.static_keyword;
 
 public class Main {
+    static double x;
+    static{
+        x=-2.575;
+    }
     public static void main(String[] args) {
         //Scenario 1: Accessing static members using className.
-        double x = -2.575;
         System.out.printf("Floor and ceil of %f is %f , %f %n",x,Math.getFloor(x),Math.getCeil(x));
         //Scenario 2: Accessing nested static class from outer class.
         Math.NestedMaths.display();
